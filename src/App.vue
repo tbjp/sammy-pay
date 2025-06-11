98d9be9e233dd7124861a04128e07abd27367957
Toby Palethorpe <tobypalethorpe@gmail.com>
2025-06-11 22:26:18 +0900

    Mobile styling

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import Star from "./assets/images/star.png";
 <template>
   <div class="flex flex-col h-dvh max-w-[480px] w-dvw overflow-hidden relative">
     <div
-      class="flex-1 flex flex-col gap-4 items-center overflow-y-auto hide-scrollbar"
+      class="flex-1 flex flex-col gap-4 items-center overflow-y-auto px-8 hide-scrollbar"
     >
       <h1 class="font-display-pixel text-2xl pt-8">Sammy Pay</h1>
       <img :src="Star" alt="Star" class="w-32" />
