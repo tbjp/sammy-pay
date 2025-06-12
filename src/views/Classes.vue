9c4133fc472debe4786cfa4cc31fc0433be880e2
Toby Palethorpe <tobypalethorpe@gmail.com>
2025-06-12 17:18:04 +0900

    Added modal and started work on the class form

--- src/views/Classes.vue
+++ src/views/Classes.vue
@@ -5,9 +5,18 @@ import SammyButton from "../components/SammyButton.vue";
 import Add from "../assets/images/icons/add.png";
 import ChevronRight from "../assets/images/icons/chevron/right.png";
 import Star from "../assets/images/star.webp";
+import Exit from "../assets/images/icons/exit.png";
 import { useRouter } from "vue-router";
+import { ref } from "vue";
+
 const appStore = useAppStore();
 const router = useRouter();
+
+const showModal = ref(false);
+
+function toggleModal() {
+  showModal.value = !showModal.value;
+}
 </script>
 
 <template>
@@ -21,6 +30,14 @@ const router = useRouter();
           alt="Add"
           class="w-full h-full"
           @click="appStore.seedData()"
+        />
+      </SammyButton>
+      <SammyButton class="w-5 h-5">
+        <img
+          :src="Add"
+          alt="Add"
+          class="w-full h-full"
+          @click="toggleModal()"
         />
       </SammyButton>
     </div>
@@ -49,5 +66,56 @@ const router = useRouter();
         </div>
       </Card>
     </ul>
+    <dialog class="modal" :class="{ 'modal-open': showModal }">
+      <div class="modal-box bg-transparent">
+        <div
+          class="bg-bg-pink min-h-24 border border-accent-navy rounded w-full"
+        >
+          <div
+            class="bg-accent-lavender border-b border-accent-navy rounded-t h-[24px] flex items-center justify-end pe-2"
+          >
+            <div class="w-4 h-4 cursor-pointer" @click="toggleModal()">
+              <img :src="Exit" alt="Close" class="w-full h-full" />
+            </div>
+          </div>
+          <div class="p-3">
+            <h3 class="text-lg font-bold mb-2">Add Class</h3>
+            <form class="flex flex-col gap-4 text-xs text-left wrap-normal">
+              <input type="datetime-local" class="sammy-input" />
+              <div class="flex gap-2">
+                <input type="number" class="sammy-input" />
+                <div class="flex-1/3">total students</div>
+              </div>
+              <div class="flex gap-2">
+                <input type="number" class="sammy-input" />
+                <div class="flex-1/3">bonus students</div>
+              </div>
+              <div class="flex gap-2">
+                <input type="number" class="sammy-input" />
+                <div class="flex-1/3">base pay per class</div>
+              </div>
+              <div class="flex gap-2">
+                <input type="number" class="sammy-input" />
+                <div class="flex-1/3">bonus pay per student</div>
+              </div>
+            </form>
+            <div class="modal-action">
+              <button class="btn" @click="toggleModal()">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <form method="dialog" class="modal-backdrop">
+        <button @click="toggleModal()">close</button>
+      </form>
+    </dialog>
   </div>
 </template>
+
+<style scoped>
+@reference "../style.css";
+
+.sammy-input {
+  @apply bg-bg-sky italic border border-accent-navy rounded p-2 flex-2/3;
+}
+</style>
