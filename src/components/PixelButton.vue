5f039c90faeed4c934bf39487f6c5ed66b046b37
Toby Palethorpe <tobypalethorpe@gmail.com>
2025-06-25 20:41:14 +0900

    stop word wrapping

--- src/components/PixelButton.vue
+++ src/components/PixelButton.vue
@@ -3,7 +3,7 @@
     <div class="relative inline-block w-content">
       <!-- Shadow behind the button -->
       <div
-        class="absolute rounded left-1 top-1 font-display-pixel px-4 py-2 text-sm bg-[#21387C] border-2 border-[#21387C] pointer-events-none"
+        class="absolute rounded left-1 top-1 font-display-pixel px-4 py-2 text-sm bg-[#21387C] border-2 border-[#21387C] pointer-events-none text-nowrap"
         aria-hidden="true"
       >
         <slot></slot>
@@ -11,7 +11,7 @@
 
       <!-- Actual button -->
       <button
-        class="relative rounded font-display-pixel px-4 py-2 text-sm text-[#21387C] bg-[#F9F4FF] border-2 border-[#21387C] hover:translate-x-1 hover:translate-y-1 transition-transform"
+        class="relative rounded font-display-pixel px-4 py-2 text-sm text-[#21387C] bg-[#F9F4FF] border-2 border-[#21387C] hover:translate-x-1 hover:translate-y-1 transition-transform text-nowrap"
       >
         <slot></slot>
       </button>
